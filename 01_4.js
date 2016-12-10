de17a56252f8e68cc82a47143b85d50a2ad18087
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:08:58 +0100

    Second prompt

--- 01_4.js
+++ 01_4.js
@@ -1,4 +1,6 @@
 var nb1;
 var nb2;
 
-nb1= prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
+nb1 = prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
+nb2 = prompt('Veuillez entrer un autre nombre décimal.');
+
