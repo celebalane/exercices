a7c29a61182248f517d706ce34d1421cbac7c7c1
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 16:15:27 +0100

    Exercice 01.6 fini

---
+++ 01_6.js
@@ -0,0 +1,6 @@
+var nom = prompt('Bonjour, quel est votre nom?');
+var prenom = prompt('Votre prénom?');
+var ville = prompt('Dans quelle ville habitez-vous?');
+
+alert('Nom : '+nom+'\nPrénom : '+prenom+ '\nVille : '+ville);
+//si les réponses n'ont pas de majuscule, le retour non plus n'en aura pas
