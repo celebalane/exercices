677b45fdc70f704f572270552e1f85c5aee689cd
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:16:13 +0100

    Ajout parseInt

--- 01_4.js
+++ 01_4.js
@@ -1,8 +1,9 @@
 var nb1;
 var nb2;
-var total = nb1*nb2;
 
 nb1 = prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
 nb2 = prompt('Veuillez entrer un autre nombre décimal.');
+
+var total = parseInt(nb1)*parseInt(nb2);
 
 alert('La multiplication de vos nombres donne : '+ total);
