39352e1d988b46dd4d77ddbbfc8f55ecb634dec4
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:20:34 +0100

    Commentaire

--- 01_4.js
+++ 01_4.js
@@ -4,6 +4,6 @@ var nb2;
 nb1 = prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
 nb2 = prompt('Veuillez entrer un autre nombre décimal.');
 
-var total = parseInt(nb1)*parseInt(nb2);
-
+var total = parseInt(nb1)*parseInt(nb2); //parseInt pour que le nombre rentré ne soit pas compris comme une string 
+//et renvoi obligatoirement un entier
 alert('La multiplication de vos nombres donne : '+ total);
