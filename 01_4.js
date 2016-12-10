3a3e3378e21842b67db9d6504d83f25a556007bf
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:13:16 +0100

    Declaration d'une variable total

--- 01_4.js
+++ 01_4.js
@@ -1,7 +1,8 @@
 var nb1;
 var nb2;
+var total = nb1*nb2;
 
 nb1 = prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
 nb2 = prompt('Veuillez entrer un autre nombre décimal.');
 
-alert('La multiplication de vos nombres donne : '+nb1*nb2);
+alert('La multiplication de vos nombres donne : '+ total);
