d6b1a69c57d0214120af7cd39beabd648de06b42
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:58:52 +0100

    Commentaire

--- 01_6.js
+++ 01_6.js
@@ -3,3 +3,4 @@ var prenom = prompt('Votre prénom?');
 var ville = prompt('Dans quelle ville habitez-vous?');
 
 alert('Nom : '+nom+'\nPrénom : '+prenom+ '\nVille : '+ville);
+//si les réponses n'ont pas de majuscule, le retour non plus n'en aura pas
