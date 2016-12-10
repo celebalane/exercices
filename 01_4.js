6fd9dbc29b385a03d1c683ea3e86ba2d693c873a
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-10 14:11:19 +0100

    essai multiplication

--- 01_4.js
+++ 01_4.js
@@ -4,3 +4,4 @@ var nb2;
 nb1 = prompt('Bonjour, veuillez entrer un nombre décimal.\nExemple : 2,678');
 nb2 = prompt('Veuillez entrer un autre nombre décimal.');
 
+alert('La multiplication de vos nombres donne : '+nb1*nb2);
