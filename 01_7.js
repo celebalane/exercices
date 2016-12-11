dcb7d1f1d8380a88076cbeb9d0a6eff6affd286a
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-11 10:42:54 +0100

    Test garçon+fille

--- 01_7.js
+++ 01_7.js
@@ -6,9 +6,19 @@ age = prompt('Quel âge avez-vous?');
 nationalité = prompt('De quelle nationalité êtes-vous?');
 sexe = prompt('Dernière question, êtes-vous une fille ou un garçon?');
 
-if (age>18,sexe = 'garçon') {
+if (age >18, sexe == 'garçon') {
 	alert('Vous êtes un garçon majeur');
 }
-else {
+else if (age <=18, sexe == 'garçon')  {
     alert('Vous êtes un garçon mineur')
 }
+else if (age >18, sexe == 'fille') {
+	alert('Vous êtes une fille majeure');
+}
+else if (age <=18, sexe == 'fille') {
+	alert('Vous êtes une fille mineure');	
+}
+else {
+	alert('Oups! Un des champs n\'a pas été rempli correctement');
+}
+
