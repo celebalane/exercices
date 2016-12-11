d0ed415e50ed2f710ea92711c519cba56022d186
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-11 10:54:55 +0100

    Ajout nationalité

--- 01_7.js
+++ 01_7.js
@@ -3,20 +3,20 @@ var nationalité;
 var sexe;
 
 age = prompt('Quel âge avez-vous?');
-nationalité = prompt('De quelle nationalité êtes-vous?');
 sexe = prompt('Dernière question, êtes-vous une fille ou un garçon?');
+nationalité = prompt('De quelle nationalité êtes-vous?');
 
 if (age >18 && sexe == 'garçon') {     //&& vérifie les deux conditions
-	alert('Vous êtes un garçon majeur');
+	alert('Vous êtes un garçon majeur de nationalité '+ nationalité);
 }
 else if (age <=18 && sexe == 'garçon')  {
-    alert('Vous êtes un garçon mineur')
+    alert('Vous êtes un garçon mineur de nationalité '+ nationalité)
 }
 else if (age >18 && sexe == 'fille') {
-	alert('Vous êtes une fille majeure');
+	alert('Vous êtes une fille majeure de nationalité '+ nationalité);
 }
 else if (age <=18 && sexe == 'fille') {
-	alert('Vous êtes une fille mineure');	
+	alert('Vous êtes une fille mineure de nationalité '+ nationalité);	
 }
 else {
 	alert('Oups! Un des champs n\'a pas été rempli correctement');
