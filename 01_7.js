c27efd01f0ffb124ef4c311ddc12c47a522ab34a
Emilie Leterme <emilie.leterme@gmail.com>
2016-12-11 10:51:19 +0100

    Ajout && pour vérifier les 2 conditions

--- 01_7.js
+++ 01_7.js
@@ -6,16 +6,16 @@ age = prompt('Quel âge avez-vous?');
 nationalité = prompt('De quelle nationalité êtes-vous?');
 sexe = prompt('Dernière question, êtes-vous une fille ou un garçon?');
 
-if (age >18, sexe == 'garçon') {
+if (age >18 && sexe == 'garçon') {     //&& vérifie les deux conditions
 	alert('Vous êtes un garçon majeur');
 }
-else if (age <=18, sexe == 'garçon')  {
+else if (age <=18 && sexe == 'garçon')  {
     alert('Vous êtes un garçon mineur')
 }
-else if (age >18, sexe == 'fille') {
+else if (age >18 && sexe == 'fille') {
 	alert('Vous êtes une fille majeure');
 }
-else if (age <=18, sexe == 'fille') {
+else if (age <=18 && sexe == 'fille') {
 	alert('Vous êtes une fille mineure');	
 }
 else {
